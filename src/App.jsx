8e85d0749d4c43b8abe9bcb0fbb964cf642afc61
IvanGodPro24 <vanyanep@gmail.com>
2025-01-15 20:59:44 +0200

    Add homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import SearchBar from "./components/SearchBar/SearchBar";
+import fetchImages from "./image-api";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import { Toaster, toast } from "react-hot-toast";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchData = async () => {
+      try {
+        setLoader(true);
+
+        const data = await fetchImages(query, page);
+
+        if (data.length === 0) {
+          toast.error("No images found for your query!", {
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+            },
+          });
+        }
+
+        setImages((prevImages) =>
+          page === 1 ? data : [...prevImages, ...data]
+        );
+
+        setHasMore(data.length > 0);
+      } catch (error) {
+        console.error(error);
+        setError(true);
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    fetchData();
+  }, [query, page]);
+
+  const handleSearch = (newQuery) => {
+    if (newQuery === query) return;
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
+    setError(false);
+  };
+
+  const nextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  function openModal(image) {
+    setSelectedImage(image);
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setSelectedImage(null);
+    setIsOpen(false);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Toaster />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSubmit={handleSearch}></SearchBar>
+      {images.length > 0 && (
+        <ImageGallery
+          images={images}
+          onClick={(image) => openModal(image)}
+        ></ImageGallery>
+      )}
+      {loader && <Loader />}
+      {error && <ErrorMessage />}
+
+      <ImageModal
+        modalIsOpen={modalIsOpen}
+        onClose={closeModal}
+        image={selectedImage}
+      />
+      {images.length > 0 && hasMore && <LoadMoreBtn onLoad={nextPage} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
